a1f3f84f764af7ed765e8b77bf9b4aa6ccb4ece1
LayaneB <50851374+LayaneB@users.noreply.github.com>
2022-07-18 15:51:44 -0300

    userRouter, ajuste user business

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { app } from './controller/app'
 import { bandRouter } from './routes/bandRouter'
+import { showRouter } from './routes/showRouter'
+import { userRouter } from './routes/userRouter'
 
-
-
-
+app.use('/user', userRouter)
 app.use('/band', bandRouter)
+app.use('/show', showRouter)
